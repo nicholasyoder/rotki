738a187fb14dab2da93f817dbba9564342cf831f
Nicholas Yoder <nicholasyoder@proton.me>
2025-12-15 14:46:49 -0500

    remove extra stuff from test

--- rotkehlchen/tests/api/test_assets_updates.py
+++ rotkehlchen/tests/api/test_assets_updates.py
@@ -903,19 +903,11 @@ INSERT INTO assets(identifier, name, type) VALUES("eip155:1/erc20:0x5dbcF33D8c2E
         weight=ONE,
     )]
 
-import logging
-
-logger = logging.getLogger(__name__)
-log = RotkehlchenLogsAdapter(logger)
 
 @pytest.mark.parametrize('use_clean_caching_directory', [True])
 @pytest.mark.parametrize('start_with_logged_in_user', [False])
 @pytest.mark.parametrize('number_of_eth_accounts', [0])
 def test_update_no_user_loggedin(rotkehlchen_api_server: 'APIServer') -> None:
-
-    x = requests.get('https://raw.githubusercontent.com/rotki/assets/develop/updates/info.json')
-    log.error(f'X_DEBUG1: {x.text}')
-
     response = requests.post(
         api_url_for(
             rotkehlchen_api_server,
