7d788d391c6b80b59cf1f67ba163fae22f5e6380
Nicholas Yoder <nicholasyoder@proton.me>
2025-12-31 15:47:39 -0500

    Group matched movement events into sublists

--- rotkehlchen/tests/api/test_event_matching.py
+++ rotkehlchen/tests/api/test_event_matching.py
@@ -13,7 +13,10 @@ from rotkehlchen.history.events.structures.base import HistoryEvent
 from rotkehlchen.history.events.structures.evm_event import EvmEvent
 from rotkehlchen.history.events.structures.swap import SwapEvent
 from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
-from rotkehlchen.tasks.events import get_unmatched_asset_movements
+from rotkehlchen.tasks.events import (
+    get_unmatched_asset_movements,
+    update_asset_movement_matched_event,
+)
 from rotkehlchen.tests.unit.test_eth2 import HOUR_IN_MILLISECONDS
 from rotkehlchen.tests.utils.api import (
     api_url_for,
@@ -281,14 +284,17 @@ def test_get_history_events_with_matched_asset_movements(
         rotkehlchen_api_server: 'APIServer',
 ) -> None:
     """Test that getting history events with matched asset movements works as expected with
-    asset movements grouped with their matched events.
+    asset movements grouped with their matched events. Checks both the case where a movement
+    is matched with an evm event and also where two movements are matched with each other.
+    The matched events (including any asset movement fee events) are grouped in a sublist to
+    make them easier to identify in the frontend.
     """
     rotki = rotkehlchen_api_server.rest_api.rotkehlchen
     dbevents = DBHistoryEvents(rotki.data.db)
     with rotki.data.db.conn.write_ctx() as write_cursor:
         dbevents.add_history_events(
             write_cursor=write_cursor,
-            history=[AssetMovement(
+            history=[(movement1 := AssetMovement(
                 identifier=1,
                 location=Location.KRAKEN,
                 event_type=HistoryEventType.WITHDRAWAL,
@@ -296,46 +302,71 @@ def test_get_history_events_with_matched_asset_movements(
                 asset=A_ETH,
                 amount=FVal('0.1'),
                 unique_id='1',
-            ), (matched_movement := AssetMovement(
+            )), AssetMovement(
                 identifier=2,
                 location=Location.KRAKEN,
                 event_type=HistoryEventType.WITHDRAWAL,
+                timestamp=movement1.timestamp,
+                asset=A_ETH,
+                amount=FVal('0.001'),
+                is_fee=True,
+                unique_id='1',
+            ), (movement2 := AssetMovement(
+                identifier=3,
+                location=Location.BINANCE,
+                event_type=HistoryEventType.DEPOSIT,
                 timestamp=TimestampMS(1500000000001),
                 asset=A_ETH,
-                amount=FVal('0.5'),
+                amount=FVal('0.1'),
                 unique_id='2',
-            )), (evm_event_1 := EvmEvent(
-                identifier=3,
-                tx_ref=(tx_ref := make_evm_tx_hash()),
-                sequence_index=0,
+            )), AssetMovement(
+                identifier=4,
+                location=Location.BINANCE,
+                event_type=HistoryEventType.DEPOSIT,
+                timestamp=movement2.timestamp,
+                asset=A_ETH,
+                amount=FVal('0.001'),
+                is_fee=True,
+                unique_id='2',
+            ), (movement3 := AssetMovement(
+                identifier=5,
+                location=Location.KRAKEN,
+                event_type=HistoryEventType.WITHDRAWAL,
                 timestamp=TimestampMS(1500000000002),
-                location=Location.ETHEREUM,
-                event_type=HistoryEventType.RECEIVE,
-                event_subtype=HistoryEventSubType.NONE,
                 asset=A_ETH,
                 amount=FVal('0.5'),
-                location_label=(user_address := make_evm_address()),
-            )), EvmEvent(
-                identifier=4,
-                tx_ref=tx_ref,
-                sequence_index=1,
+                unique_id='3',
+            )), EvmEvent(  # event in tx but unrelated to the movement. Could be gas fee, etc.
+                identifier=6,
+                tx_ref=(tx_ref := make_evm_tx_hash()),
+                sequence_index=0,
                 timestamp=TimestampMS(1500000000002),
                 location=Location.ETHEREUM,
                 event_type=HistoryEventType.SPEND,
                 event_subtype=HistoryEventSubType.NONE,
                 asset=A_ETH,
                 amount=FVal('0.01'),
+                location_label=(user_address := make_evm_address()),
+            ), (evm_event_1 := EvmEvent(
+                identifier=7,
+                tx_ref=tx_ref,
+                sequence_index=1,
+                timestamp=TimestampMS(1500000000002),
+                location=Location.ETHEREUM,
+                event_type=HistoryEventType.RECEIVE,
+                event_subtype=HistoryEventSubType.NONE,
+                asset=A_ETH,
+                amount=FVal('0.5'),
                 location_label=user_address,
-            )],
+            ))],
         )
 
-        assert matched_movement.identifier is not None
-        assert evm_event_1.identifier is not None
-        rotki.data.db.set_dynamic_cache(
-            write_cursor=write_cursor,
-            name=DBCacheDynamic.MATCHED_ASSET_MOVEMENT,
-            identifier=matched_movement.identifier,
-            value=evm_event_1.identifier,
+    for asset_movement, matched_event in ((movement1, movement2), (movement3, evm_event_1)):
+        update_asset_movement_matched_event(
+            events_db=dbevents,
+            asset_movement=asset_movement,
+            matched_event=matched_event,
+            is_deposit=False,
         )
 
     # First query history aggregated by group
@@ -350,7 +381,8 @@ def test_get_history_events_with_matched_asset_movements(
     assert len(result['entries']) == 2
     assert result['entries'][0]['grouped_events_num'] == 3  # includes both evm events and the matched asset movement  # noqa: E501
     assert result['entries'][0]['entry']['group_identifier'] == evm_event_1.group_identifier
-    assert result['entries'][1]['grouped_events_num'] == 1  # the unmatched asset movement
+    assert result['entries'][1]['grouped_events_num'] == 4  # the two matched movements and their fees  # noqa: E501
+    assert result['entries'][1]['entry']['group_identifier'] == movement2.group_identifier
 
     # Then query the evm event group and the matched asset movement should be included
     result = assert_proper_response_with_result(
@@ -363,18 +395,52 @@ def test_get_history_events_with_matched_asset_movements(
         ),
         rotkehlchen_api_server=rotkehlchen_api_server,
     )['entries']
-
-    # The group returned should all have the group_identifier of the evm event, but should also
+    # The events returned should all have the group_identifier of the evm event, but should also
     # have the actual_group_identifier set to preserve the real group_identifier of each event.
     # The presence of this field also notifies frontend that this group is a combination of
     # several groups and may need special handling.
-    assert len(result) == 3  # receive and spend onchain events plus the asset movement
-    assert result[0]['entry']['event_type'] == 'receive'
-    assert result[0]['entry']['group_identifier'] == evm_event_1.group_identifier
-    assert result[0]['entry']['actual_group_identifier'] == evm_event_1.group_identifier
-    assert result[1]['entry']['event_type'] == 'spend'
-    assert result[1]['entry']['group_identifier'] == evm_event_1.group_identifier
-    assert result[1]['entry']['actual_group_identifier'] == evm_event_1.group_identifier
-    assert result[2]['entry']['event_type'] == 'withdrawal'
-    assert result[2]['entry']['group_identifier'] == evm_event_1.group_identifier
-    assert result[2]['entry']['actual_group_identifier'] == matched_movement.group_identifier
+    assert len(result) == 2
+    # The first event in the tx isn't related to the asset movement
+    assert (unrelated_event_entry := result[0])['entry']['event_type'] == 'spend'
+    assert unrelated_event_entry['entry']['group_identifier'] == evm_event_1.group_identifier
+    assert unrelated_event_entry['entry']['actual_group_identifier'] == evm_event_1.group_identifier  # noqa: E501
+    # matched events are in a sublist so frontend can easily group them
+    assert len(match1_sublist := result[1]) == 2
+    assert match1_sublist[0]['entry']['event_type'] == 'deposit'
+    assert match1_sublist[0]['entry']['group_identifier'] == evm_event_1.group_identifier
+    assert match1_sublist[0]['entry']['actual_group_identifier'] == evm_event_1.group_identifier
+    assert match1_sublist[1]['entry']['event_type'] == 'withdrawal'
+    assert match1_sublist[1]['entry']['group_identifier'] == evm_event_1.group_identifier
+    assert match1_sublist[1]['entry']['actual_group_identifier'] == movement3.group_identifier
+
+    # Then check the events for the second matched event (movement2)
+    result = assert_proper_response_with_result(
+        response=requests.post(api_url_for(rotkehlchen_api_server, 'historyeventresource'), json={
+            'aggregate_by_group_ids': False,
+            'group_identifiers': [movement2.group_identifier],
+        }),
+        rotkehlchen_api_server=rotkehlchen_api_server,
+    )['entries']
+    assert len(result) == 1
+    assert len(match2_sublist := result[0]) == 4
+    assert all(x['entry']['group_identifier'] == movement2.group_identifier for x in match2_sublist)  # noqa: E501
+    assert match2_sublist[0]['entry']['event_type'] == 'deposit'
+    assert match2_sublist[0]['entry']['actual_group_identifier'] == movement2.group_identifier
+    assert match2_sublist[1]['entry']['event_subtype'] == 'fee'
+    assert match2_sublist[1]['entry']['actual_group_identifier'] == movement2.group_identifier
+    assert match2_sublist[2]['entry']['event_type'] == 'withdrawal'
+    assert match2_sublist[2]['entry']['actual_group_identifier'] == movement1.group_identifier
+    assert match2_sublist[3]['entry']['event_subtype'] == 'fee'
+    assert match2_sublist[3]['entry']['actual_group_identifier'] == movement1.group_identifier
+
+    # Check that querying both groups at once works correctly
+    assert assert_proper_response_with_result(
+        response=requests.post(
+            api_url_for(rotkehlchen_api_server, 'historyeventresource'),
+            json={
+                'aggregate_by_group_ids': False,
+                'group_identifiers': [movement2.group_identifier, evm_event_1.group_identifier],
+            },
+        ),
+        rotkehlchen_api_server=rotkehlchen_api_server,
+    )['entries'] == [unrelated_event_entry, match1_sublist, match2_sublist]
