a1228cc4a789221f996aa10a9c704210f2a908cd
Isaac Adewumi <ienioladewumi@gmail.com>
2026-02-05 05:07:26 +0100

    add endpoint to enable/disable periodic tasks scheduler (#11498)

--- rotkehlchen/api/v1/schemas.py
+++ rotkehlchen/api/v1/schemas.py
@@ -4755,5 +4755,9 @@ class TriggerTaskSchema(AsyncQueryArgumentSchema):
     task = SerializableEnumField(enum_class=TaskName, required=True)
 
 
+class SchedulerSchema(Schema):
+    enabled = fields.Boolean(required=True)
+
+
 class CustomizedEventDuplicatesFixSchema(AsyncQueryArgumentSchema):
     group_identifiers = fields.List(NonEmptyStringField(), load_default=None)
