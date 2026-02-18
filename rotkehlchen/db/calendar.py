93b151f0106e18f27dd65fc6dab078652a9f0c4f
Lefteris Karapetsas <lefteris@refu.co>
2026-02-18 20:46:17 +0000

    fix: raise on missing calendar reminder update

--- rotkehlchen/db/calendar.py
+++ rotkehlchen/db/calendar.py
@@ -313,6 +313,7 @@ class DBCalendar:
                 )
             except sqlcipher.IntegrityError as e:  # pylint: disable=no-member
                 raise InputError(f'Could not update calendar entry due to {e}') from e
+
             if write_cursor.rowcount == 0:
                 raise InputError(f'Tried to update a non existent calendar entry {calendar.identifier}')  # noqa: E501
 
@@ -354,6 +355,9 @@ class DBCalendar:
                 )
             except sqlcipher.IntegrityError as e:  # pylint: disable=no-member
                 raise InputError(f'Could not update reminder due to {e}') from e
+
+            if write_cursor.rowcount == 0:
+                raise InputError(f'Tried to update a non existent reminder {reminder.identifier}')
 
         return reminder.identifier
 
--- rotkehlchen/tests/db/test_calendar.py
+++ rotkehlchen/tests/db/test_calendar.py
@@ -1,6 +1,6 @@
 import pytest
 
-from rotkehlchen.db.calendar import CalendarEntry, DBCalendar
+from rotkehlchen.db.calendar import CalendarEntry, DBCalendar, ReminderEntry
 from rotkehlchen.errors.misc import InputError
 from rotkehlchen.types import Timestamp
 
@@ -19,4 +19,16 @@ def test_update_nonexistent_calendar_entry_raises(database) -> None:
             color=None,
             auto_delete=False,
             identifier=999999,
+        ))
+
+
+def test_update_nonexistent_reminder_entry_raises(database) -> None:
+    db_calendar = DBCalendar(database)
+
+    with pytest.raises(InputError, match='non existent reminder'):
+        db_calendar.update_reminder_entry(ReminderEntry(
+            identifier=999999,
+            event_id=1,
+            secs_before=3600,
+            acknowledged=False,
         ))
